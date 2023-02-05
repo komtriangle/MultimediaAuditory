a55810c9c2a15eb939ce1e215fbb63cb6508188c
komtriangle <komlevdanila742@gmail.com>
2023-02-05 18:12:53 +0300

    plugin

--- Frontend/plugins/sberDevices.Plugin/src/android/sberDevicesPlugins.kt
+++ Frontend/plugins/sberDevices.Plugin/src/android/sberDevicesPlugins.kt
@@ -1,53 +1,51 @@
 package sberDevices.Plugin
 
-import org.apache.cordova.CordovaPlugin
 import android.app.Application
 import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaPlugin
 import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
-import ru.sberdevices.messaging.MessageId
-import ru.sberdevices.messaging.MessageName
-import ru.sberdevices.messaging.Messaging
-import ru.sberdevices.messaging.Payload
-import ru.sberdevices.services.appstate.AppStateManagerFactory
-import ru.sberdevices.services.assistant.PublicAssistantFactory
 import ru.sberdevices.common.assert.Asserts
 import ru.sberdevices.common.binderhelper.BinderHelperFactory2
 import ru.sberdevices.common.binderhelper.BinderHelperFactory2Impl
-import ru.sberdevices.common.coroutines.CoroutineDispatchers
 import ru.sberdevices.common.logger.AndroidLoggerDelegate
 import ru.sberdevices.common.logger.Logger
-import ru.sberdevices.messaging.MessagingFactory
+import ru.sberdevices.messaging.MessageId
+import ru.sberdevices.messaging.Messaging
+//import ru.sberdevices.services.appstate.AppStateHolder
+import ru.sberdevices.messaging.Payload
+import ru.sberdevices.services.appstate.AppStateManagerFactory
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.slot
 
 
 /**
  * This class echoes a string called from JavaScript.
  */
-class sberDevicesPlugins : CordovaPlugin() {
+class sberDevicesPlugins(messaging: Messaging) : CordovaPlugin() {
 
-    private val messaging: Messaging = mockk()
-
-    init {
-        AppStateManagerFactory.createHolder()
-        MessagingFactory.create()
-    }
-
-
-    var listener: Messaging.Listener = object : Messaging.Listener {
+    private  val listener =  object : Messaging.Listener {
         override fun onMessage(messageId: MessageId, payload: Payload) {
             //val sberMessage = Gson().fromJson(
             //					payload.data,
             //					SberMessage::class.java
             //			)
 
+            print("Bye world...")
+            //callbackContext.success(payload.data);
+
         }
 
         override fun onError(messageId: MessageId, throwable: Throwable) {
-            // Пришла ошибка: ~~будем плакать~~ обработаем ее
+            //callbackContext.error("ERROR");
         }
     }
+    init {
+        messaging.addListener(listener)
+    }
+
 
     override fun execute(
         action: String,
@@ -57,22 +55,23 @@ class sberDevicesPlugins : CordovaPlugin() {
         if (true) {
             val message: String = args.getString(0)
             coolMethod(message, callbackContext)
-            listener = object : Messaging.Listener {
-                override fun onMessage(messageId: MessageId, payload: Payload) {
-                    //val sberMessage = Gson().fromJson(
-                    //					payload.data,
-                    //					SberMessage::class.java
-                    //			)
-                    callbackContext.success(payload.data);
+//            listener = object : Messaging.Listener {
+//                override fun onMessage(messageId: MessageId, payload: Payload) {
+//                    //val sberMessage = Gson().fromJson(
+//                    //					payload.data,
+//                    //					SberMessage::class.java
+//                    //			)
+//                    callbackContext.success(payload.data);
+//
+//                }
+//
+//                override fun onError(messageId: MessageId, throwable: Throwable) {
+//                    callbackContext.error("ERROR");
+//                }
+//            }
+           // every { messaging.addListener(listener = listener) } just Runs
 
-                }
 
-                override fun onError(messageId: MessageId, throwable: Throwable) {
-                    callbackContext.error("ERROR");
-                }
-            }
-
-            messaging.addListener(listener)
 
             return true
         }
@@ -90,4 +89,5 @@ class sberDevicesPlugins : CordovaPlugin() {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
 }
